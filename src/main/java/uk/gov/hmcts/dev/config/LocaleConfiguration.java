d6b1e590cb9783ca579656826452ba91f83803e4
uche.umeevuruo@creditville.ng <Dental123#>
2025-08-23 16:28:44 +0100

    SCRUM-3: Added language internalization. Added for US, Spanish and French

--- src/main/java/uk/gov/hmcts/dev/config/LocaleConfiguration.java
+++ src/main/java/uk/gov/hmcts/dev/config/LocaleConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
